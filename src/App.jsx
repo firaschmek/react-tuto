3e8312055ab15cb22e72394633a234e158cf69c9
firas.chemak@dti.ulaval.ca <firas.chemak@dti.ulaval.ca>
2024-01-20 18:26:59 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+import { CORE_CONCEPTS } from "./data.js";
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 4));
+}
+
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are going to build!
+      </p>
+    </header>
+  );
+}
+
+function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>
+        {description}
+      </p>
+    </li>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
